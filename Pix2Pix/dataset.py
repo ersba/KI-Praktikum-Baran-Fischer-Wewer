3f5fb8e7250816de52db8490bb485b246dbe3eeb
Dennis <94540620+Dennisfischer0711@users.noreply.github.com>
2024-06-20 19:19:27 +0200

    Update dataset.py
    Instead of one image entire dataset is now integrated

--- Pix2Pix/dataset.py
+++ Pix2Pix/dataset.py
@@ -5,49 +5,40 @@ from PIL import Image
 from torch.utils.data import Dataset, DataLoader
 from torchvision.utils import save_image
 
-
 class MapDataset(Dataset):
     def __init__(self, root_dir):
-        self.root_dir = root_dir
-        self.list_files = os.listdir(self.root_dir)
+        self.wildfire_dir = os.path.join(root_dir, "wildfire")
+        self.nowildfire_dir = os.path.join(root_dir, "nowildfire")
+        self.list_files = os.listdir(self.wildfire_dir)
 
     def __len__(self):
         return len(self.list_files)
 
     def __getitem__(self, index):
         img_file = self.list_files[index]
-        img_path = os.path.join(self.root_dir, img_file)
-        image = np.array(Image.open(img_path))
-        input_image = image[:, :350, :]
-        target_image = image[:, 350:, :]
+        wildfire_path = os.path.join(self.wildfire_dir, img_file)
+        nowildfire_path = os.path.join(self.nowildfire_dir, img_file)
 
-        augmentations = config.both_transform(image=input_image, image0=target_image)
-        input_image = augmentations["image"]
-        target_image = augmentations["image0"]
+        wildfire_image = np.array(Image.open(wildfire_path).convert("RGB"))
+        nowildfire_image = np.array(Image.open(nowildfire_path).convert("RGB"))
 
-        input_image = config.transform_only_input(image=input_image)["image"]
-        target_image = config.transform_only_mask(image=target_image)["image"]
+        augmentations = config.both_transform(image=nowildfire_image, image0=wildfire_image)
+        nowildfire_image = augmentations["image"]
+        wildfire_image = augmentations["image0"]
 
-        return input_image, target_image
+        nowildfire_image = config.transform_only_input(image=nowildfire_image)["image"]
+        wildfire_image = config.transform_only_mask(image=wildfire_image)["image"]
 
-def save_normalized_image(img, path):
-    # Normierung in den Bereich [0, 1]
-    img = (img - img.min()) / (img.max() - img.min())
-    # Konvertierung zu float
-    img = img.float()
-    save_image(img, path)
-
+        return nowildfire_image, wildfire_image
 
 if __name__ == "__main__":
     current_file_path = os.path.abspath(__file__)
     current_folder = os.path.dirname(current_file_path)
-    dataset = MapDataset(os.path.join(current_folder, "data"))
-    print(current_folder)
+    dataset = MapDataset(os.path.join(current_folder, "..", "actual_dataset", "train"))
     loader = DataLoader(dataset, batch_size=1)
     for x, y in loader:
         print(x.shape)
         save_image(x, "x.png")
         save_image(y, "y.png")
         import sys
-
         sys.exit()
