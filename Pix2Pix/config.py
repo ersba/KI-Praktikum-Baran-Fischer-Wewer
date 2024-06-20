79fd7c4c15495b4b516918e53759d1ccad828ac8
Dennis <94540620+Dennisfischer0711@users.noreply.github.com>
2024-06-20 19:19:54 +0200

    Update config.py
    Normalization adapted

--- Pix2Pix/config.py
+++ Pix2Pix/config.py
@@ -1,39 +1,40 @@
-import torch
 import albumentations as A
+import torch
 from albumentations.pytorch import ToTensorV2
 
+
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
-TRAIN_DIR = "data"
-VAL_DIR = "data"
+BASE_DIR = "D:\\neu_studium\\Master_Semester_01\\KI\\KI_Script_2_0"
+TRAIN_DIR = f"{BASE_DIR}\\actual_dataset\\train"
+VAL_DIR = f"{BASE_DIR}\\actual_dataset\\val"
+TEST_DIR = f"{BASE_DIR}\\actual_dataset\\test"
 LEARNING_RATE = 2e-4
-BATCH_SIZE = 1
-NUM_WORKERS = 2
-IMAGE_SIZE = 350
+BATCH_SIZE = 16
+NUM_WORKERS = 4
+IMAGE_SIZE = 256
 CHANNELS_IMG = 3
 L1_LAMBDA = 100
 LAMBDA_GP = 10
 NUM_EPOCHS = 100
 LOAD_MODEL = False
-SAVE_MODEL = False
-CHECKPOINT_DISC = "disc.pth.tar"
-CHECKPOINT_GEN = "gen.pth.tar"
+SAVE_MODEL = True
+CHECKPOINT_DISC = "disc.pth"
+CHECKPOINT_GEN = "gen.pth"
 
 both_transform = A.Compose(
-    [A.Resize(width=256, height=256),], additional_targets={"image0": "image"},
+    [A.Resize(width=IMAGE_SIZE, height=IMAGE_SIZE)], additional_targets={"image0": "image"},
 )
 
 transform_only_input = A.Compose(
     [
-        # A.HorizontalFlip(p=0.5), makes no sense with out problem
-        # A.ColorJitter(p=0.2),
-        A.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5], max_pixel_value=255.0,),
+        A.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5]),
         ToTensorV2(),
     ]
 )
 
 transform_only_mask = A.Compose(
     [
-        A.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5], max_pixel_value=255.0,),
+        A.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5]),
         ToTensorV2(),
     ]
 )
